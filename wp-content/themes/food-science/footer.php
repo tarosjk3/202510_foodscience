a22a347eaeda522703d054e2e33784949a8ceb8e
tarosjk3 <tarosjk3@gmail.com>
2026-02-12 13:51:07 +0900

    TOPページ用のSlick関連スタイルとスクリプトの分岐

--- wp-content/themes/food-science/footer.php
+++ wp-content/themes/food-science/footer.php
@@ -26,10 +26,13 @@
     <a href="#"><i class="fas fa-arrow-up"></i><span>TOP PAGE</span></a>
   </div>
 
-  <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css" />
-  <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-  <script type="text/JavaScript" src="<?= get_template_directory_uri(); ?>/assets/js/home.js"></script>
-
+  <?php 
+    if(is_home()) {
+        wp_enqueue_style('slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css');
+        wp_enqueue_script('slick-carousel', 'https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js');
+        wp_enqueue_script('food-science-home', get_template_directory_uri() . '/assets/js/home.js');
+    }
+  ?>
   <?php wp_footer(); ?>
 </body>
 </html>
