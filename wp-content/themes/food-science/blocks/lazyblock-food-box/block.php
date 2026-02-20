16a3ffe1f831abff13a1a78d9851f27d3a989282
tarosjk3 <tarosjk3@gmail.com>
2026-02-20 14:41:46 +0900

    カスタムブロック（lazy_blocks）のテンプレート設定

---
+++ wp-content/themes/food-science/blocks/lazyblock-food-box/block.php
@@ -0,0 +1,9 @@
+<?php
+
+// var_dump($attributes);
+
+?>
+<p><?= $attributes['pic'];  ?></p>
+<p><?= $attributes['name'];  ?></p>
+<p><?= $attributes['price'];  ?></p>
+<p><?= $attributes['url'];  ?></p>
